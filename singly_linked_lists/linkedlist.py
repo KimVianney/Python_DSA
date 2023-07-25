e0f00b21cbaecf7fcd8be4c53dccf11b3676b9dd
Vianney Kimuri <kimvianney6@gmail.com>
2023-07-25 14:05:38 +0300

    Add code for node deletion in linked list

--- singly_linked_lists/linkedlist.py
+++ singly_linked_lists/linkedlist.py
@@ -38,6 +38,55 @@ class LinkedList:
         new_node.next = prev_node.next
         prev_node.next = new_node
 
+    def delete_node(self, key):
+        """
+        Delete a node based on the values it stores in the 
+        linked list
+        """
+        cur_node = self.head
+        if cur_node and cur_node.data == key:
+            self.head = cur_node.next
+            cur_node = None
+            return
+
+        prev = None
+        while cur_node and cur_node.data != key:
+            prev = cur_node
+            cur_node = cur_node.next
+
+        if cur_node is None:
+            return
+
+        prev.next = cur_node.next
+        cur_node = None
+
+    def delete_node_at_pos(self, pos):
+        """
+        Delete a node based on its position in the 
+        linked list
+        Take note: Position begins at 0
+        """
+        if self.head:
+            cur_node = self.head
+            if pos == 0:
+                self.head = cur_node.next
+                cur_node = None
+                return 
+
+            prev = None
+            count = 0 
+            while cur_node and count != pos:
+                prev = cur_node
+                cur_node = cur_node.next
+                count += 1
+
+            if cur_node is None:
+                return 
+
+            prev.next = cur_node.next
+            cur_node = None
+
+
 
 
 ## Test it out
@@ -49,6 +98,9 @@ llist.append('D')
 llist.prepend('E')
 
 llist.insert_after_node(llist.head.next.next, "G")
-
+llist.delete_node('E')
+llist.delete_node('G')
+llist.delete_node_at_pos(3)
+llist.delete_node_at_pos(0)
 
 llist.print_list()
